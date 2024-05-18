a53f0b2c700d3c36486f1d4c4b9fd78af0c9756e
박익범 <parkikbum@bag-igbeom-ui-MacBookPro-2.local>
2024-05-18 18:58:07 +0900

    Chore: UIView 익스텐션 제거 및 extension package 추가

--- Sopkathon-iOS/Sopkathon-iOS/Global/Extension/UIView+.swift
+++ Sopkathon-iOS/Sopkathon-iOS/Global/Extension/UIView+.swift
@@ -1,14 +1,1 @@
-//
-//  UIView+.swift
-//  Sopkathon-iOS
-//
-//  Created by 이명진 on 5/16/24.
-//
 
-import UIKit
-
-extension UIView {
-    func addSubviews(_ views: UIView...) {
-        views.forEach { self.addSubview($0) }
-    }
-}
