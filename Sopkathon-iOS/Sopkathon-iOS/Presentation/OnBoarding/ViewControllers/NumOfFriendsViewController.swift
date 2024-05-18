9113b9728569d276736b9a51b5bee082d1a9760e
kms0233 <cnblue789@naver.com>
2024-05-19 07:12:17 +0900

    [Fix] #28 - 로고 추가

--- Sopkathon-iOS/Sopkathon-iOS/Presentation/OnBoarding/ViewControllers/NumOfFriendsViewController.swift
+++ Sopkathon-iOS/Sopkathon-iOS/Presentation/OnBoarding/ViewControllers/NumOfFriendsViewController.swift
@@ -21,6 +21,10 @@ final class NumOfFriendsViewController: UIViewController {
     
     private var nickName: String?
     
+    private let logoImageView = UIImageView().then {
+        $0.image = .logo
+    }
+    
     private let characterImageView = UIImageView().then {
         $0.image = .imgOnboarding2
     }
@@ -82,15 +86,20 @@ final class NumOfFriendsViewController: UIViewController {
 extension NumOfFriendsViewController {
     
     func setHierarchy() {
-        view.addSubviews(characterImageView,friendsLabel, numStackView, completeButton)
+        view.addSubviews(logoImageView,characterImageView,friendsLabel, numStackView, completeButton)
         
     }
     
     func setLayout() {
         self.view.backgroundColor = .background
-        //navigationItem.hidesBackButton = true
+        navigationItem.hidesBackButton = true
         
-        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(70)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(161)
+            $0.height.equalTo(28)
+        }
         friendsLabel.snp.makeConstraints {
             $0.top.equalToSuperview().inset(250)
             $0.leading.trailing.equalToSuperview()
