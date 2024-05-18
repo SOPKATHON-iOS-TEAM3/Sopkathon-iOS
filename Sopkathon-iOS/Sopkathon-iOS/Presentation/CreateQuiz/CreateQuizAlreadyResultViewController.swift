242034b91e53a12021e5fd66ef21ce190dc3f521
parkikbum <pibum@naver.com>
2024-05-19 05:15:32 +0900

    [Feat] #14 - add already create result view

---
+++ Sopkathon-iOS/Sopkathon-iOS/Presentation/CreateQuiz/CreateQuizAlreadyResultViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+import SnapKit
+import Sopkathon_iOS_Extension
+import Then
+
+
+class CreateQuizAlreadyResultViewController: UIViewController {
+    // MARK: properties
+    // MARK: - UIComponents
+    private let createQuizAlreadyResultView = CreateAlreadyQuizResultView()
+    // MARK: - Life Cycles
+    override func loadView() {
+        super.loadView()
+        self.view = createQuizAlreadyResultView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - UI & Layout
+    // MARK: - Methods
+    // MARK: - Data Source
+    // MARK: - Delegate
+    // MARK: - Actions
+
+}
