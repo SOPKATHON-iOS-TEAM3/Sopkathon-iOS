46633a23f74e3a6f74ff04e8c9e57bb8532a0abe
박익범 <parkikbum@bag-igbeom-ui-MacBookPro-2.local>
2024-05-19 02:51:19 +0900

    [Feat] #4 - 답변/퀴즈  텍스트 필드 및 텍스트 뷰 구현

--- Sopkathon-iOS/Sopkathon-iOS/Presentation/CreateQuiz/Components/InputQuestionView.swift
+++ Sopkathon-iOS/Sopkathon-iOS/Presentation/CreateQuiz/Components/InputQuestionView.swift
@@ -1,8 +1,51 @@
-//
-//  InputQuestionView.swift
-//  Sopkathon-iOS
-//
-//  Created by 박익범 on 5/19/24.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Sopkathon_iOS_Extension
+import Then
+
+
+final class InputQuestionView: UIView {
+    // MARK: properties
+    var isEditing: Bool = false {
+        didSet {
+            placeHolderLabel.isHidden = isEditing
+        }
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setLayout()
+        setUI()
+    }
+    // MARK: - UIComponents
+    let textView = UITextView()
+    let placeHolderLabel = UILabel().then {
+        $0.textAlignment = .left
+    }
+    // MARK: - Life Cycles
+    // MARK: - UI & Layout
+    private func setUI() {
+        backgroundColor = .white
+    }
+    private func setLayout() {
+        self.addSubviews(textView,
+                         placeHolderLabel)
+        
+        textView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(10)
+            $0.leading.trailing.equalToSuperview().inset(14)
+        }
+        
+        placeHolderLabel.snp.makeConstraints {
+            $0.top.leading.equalTo(textView)
+        }
+    }
+    // MARK: - Methods
+    // MARK: - Data Source
+    // MARK: - Delegate
+    // MARK: - Actions
+}
--- Sopkathon-iOS/Sopkathon-iOS/Presentation/CreateQuiz/Components/PaddingLabel.swift
+++ Sopkathon-iOS/Sopkathon-iOS/Presentation/CreateQuiz/Components/PaddingLabel.swift
@@ -1,8 +1,27 @@
-//
-//  PaddingLabel.swift
-//  Sopkathon-iOS
-//
-//  Created by 박익범 on 5/19/24.
-//
+import UIKit
 
-import Foundation
+class PaddingLabel: UILabel {
+    // MARK: properties
+    private var padding = UIEdgeInsets(top: 0, left: 5.0, bottom: 0, right: 0)
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.padding = padding
+    }
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += padding.top + padding.bottom
+        contentSize.width += padding.left + padding.right
+
+        return contentSize
+    }
+    // MARK: - UIComponents
+    // MARK: - Life Cycles
+    // MARK: - UI & Layout
+    // MARK: - Methods
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
+    }
+    // MARK: - Data Source
+    // MARK: - Delegate
+    // MARK: - Actions
+}
